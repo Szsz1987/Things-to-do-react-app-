ee0bc973ffb83bfdef18e79e973cefa4889fe16c
Szymon Szuliński <sszulinski@gmail.com>
2022-03-12 07:26:46 +0100

    Add data types & passing props from App.js to Hero.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Hero from '../Hero/Hero';
 import styles from './List.scss';
+import PropTypes from 'prop-types';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    source: PropTypes.string,
+  }
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>
+  }
   render() {
     return (
       <section className={styles.component}>
-        <Hero/>
+        <Hero titleText={this.props.title} imageSource={this.props.source} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
       </section>
     )
   }
