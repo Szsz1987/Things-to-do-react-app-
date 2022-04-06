67d71df368da06e73f1867e64777c942b8f1f9df
Szymon Szuliński <sszulinski@gmail.com>
2022-04-06 19:39:52 +0200

    Add Creator of lists in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './App.scss';
-import {pageContents, listData} from '../../data/dataStore';
+import {pageContents, listData, settings} from '../../data/dataStore';
 import List from '../List/List';
 import PropTypes from 'prop-types';
+import Creator from '../Creator/Creator';
 
 class App extends React.Component {
   state = {
@@ -28,6 +29,9 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>{pageContents.title}</h1>
         <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+        <div className={styles.creator}>
+          <Creator text={settings.listCreatorText} action={title => this.addList(title)}/>
+        </div>
         {this.state.lists.map(
           ({key, ...listProps}) => (
             <List key={key} {...listProps}/>
