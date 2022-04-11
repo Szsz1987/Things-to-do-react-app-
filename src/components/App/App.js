c371f96abba2aaa35071e43a42c6ef6cbc26bfea
Szymon Szuliński <sszulinski@gmail.com>
2022-04-11 21:12:15 +0200

    Implement Redux, first changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,42 +1,24 @@
 import React from 'react';
 import styles from './App.scss';
-import {pageContents, listData, settings} from '../../data/dataStore';
-import List from '../List/List';
+import List from '../List/ListContainer';
 import PropTypes from 'prop-types';
-import Creator from '../Creator/Creator';
 
 class App extends React.Component {
-  state = {
-    lists: [listData],
-  }
-  addList(title) {
-    this.setState(state => (
-      {
-        lists: [
-          ...state.lists, 
-          {
-            title,
-            columns: [],
-            description: 'Interesting things I want to check out!',
-            image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
-          },
-        ],
-      }
-    ));
+  static propTypes = {
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
+    lists: PropTypes.array,
   }
   render() {
+    const {title, subtitle, lists} = this.props;
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>{pageContents.title}</h1>
-        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-        <div className={styles.creator}>
-          <Creator text={settings.listCreatorText} action={title => this.addList(title)}/>
-        </div>
-        {this.state.lists.map(
-          ({key, ...listProps}) => (
-            <List key={key} {...listProps}/>
-          )
-          )}
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+        {lists.map(listData => (
+          <List key={listData.id} {...listData}/>
+        )
+        )}
       </main>
     );
   }
