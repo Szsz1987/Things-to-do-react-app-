64387bc65721b3aed93f4b6fdb5022e2017d7c31
Szymon Szuliński <sszulinski@gmail.com>
2022-07-09 19:49:57 +0200

    Add container to the component List and Search

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,37 +6,38 @@ import Creator from '../Creator/Creator';
 import PropTypes from 'prop-types';
 import {listData, settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
+import Container from '../Container/Container';
 
-class List extends React.Component {
-  static propTypes = {
-    title: PropTypes.node.isRequired,
-    description: PropTypes.node,
-    columns: PropTypes.array,
-    image: PropTypes.string,
-    addColumn: PropTypes.func,
-  }
-  static defaultProps = {
-    description: settings.defaultListDescription,
-    source: listData.image,
-  }
-  render() {
-    const {title, image, description, columns, addColumn} = this.props;
-    return (
-      <section className={styles.component}>
-        <Hero titleText={title} imageSource={image} />
-        <div className={styles.description}>
-          {ReactHtmlParser(description)}
-        </div>
-        <div className={styles.columns}>
-          {columns.map(columnData => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
-        <div className={styles.creator}>
-          <Creator text={settings.columnCreatorText} action={addColumn}/>
-        </div>
-      </section>
-    );
-  }
-}
+const List = ({title, description, columns, image, addColumn}) => (
+  <Container>
+    <section className={styles.component}>
+      <Hero titleText={title} imageSource={image} />
+      <div className={styles.description}>
+        {ReactHtmlParser(description)}
+      </div>
+      <div className={styles.columns}>
+        {columns.map(columnData => (
+          <Column key={columnData.id} {...columnData}/>
+        ))}
+      </div>
+      <div className={styles.creator}>
+        <Creator text={settings.columnCreatorText} action={addColumn}/>
+      </div>
+    </section>
+  </Container>
+);
+
+List.defaultProps = {
+  description: settings.defaultListDescription,
+  source: listData.image,
+};
+
+List.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  columns: PropTypes.array,
+  image: PropTypes.string,
+  addColumn: PropTypes.func,
+};
+
 export default List;
