170ac63bc99ac7de4a43455b487de401b77d1793
Szymon Szuliński <sszulinski@gmail.com>
2022-09-15 21:34:40 +0200

    Add the ListLink component to the Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Home.scss';
-import List from '../List/ListContainer';
+import ListLink from '../ListLink/ListLink';
 import PropTypes from 'prop-types';
 import Search from '../Search/SearchContainer';
 
@@ -18,7 +18,7 @@ class Home extends React.Component {
         <h2 className={styles.subtitle}>{subtitle}</h2>
         <Search/>
         {lists.map(listData => (
-          <List key={listData.id} {...listData} />
+          <ListLink key={listData.id} {...listData} />
         ))}
       </main>
     );
